911ba81c4b19015e7f9a180c2c5774b5ae1bd6b3
Kurtis Pivert, MS, CAP <kurtis.pivert@gmail.com>
2025-02-25 23:51:29 +0000

    ffs

--- models/core/fct_taxi_trips_quarterly_revenue.sql
+++ models/core/fct_taxi_trips_quarterly_revenue.sql
@@ -5,13 +5,15 @@ with trips_data as (
 )
     select 
     -- Revenue grouping 
-    extract(year from pickup_datetime) as revenue_year,
+    {{ dbt.date_trunc("month", "pickup_datetime") }} as revenue_month, 
+    {{ dbt.date_trunc("quarter", "pickup_datetime") }} as revenue_quarter, 
+    {{ dbt.date_trunc("year", "pickup_datetime") }} as revenue_year,
+
+    service_type, 
 
     -- Revenue calculation 
+    sum(total_amount) as revenue_monthly_total_amount,
 
-    sum(total_amount) as revenue_quarterly_total_amount
-
-    -- Additional calculations
 
     from trips_data
-    group by revenue_year
+    group by revenue_month
